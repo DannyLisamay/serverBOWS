86d386e8959e75de76d050d10b76a4a09c9cd324
Danny Lisamay <DannyLisamay@gmail.com>
2021-04-13 19:46:24 -0400

    Updated. Creating RESTful api for backend.

--- app.js
+++ app.js
@@ -5,31 +5,47 @@
 require('dotenv').config();
 // Express js
 const express = require("express");
-// Body parser
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-// Lodash
-const _ = require("lodash");
 // MongoDB
 const mongoose = require("mongoose");
-// Local port 3000 used for testing.
-const port = 3000;
-
+const { lowerFirst } = require('lodash');
+// Local port 4000 used for testing.
+const port = 4003;
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
 // Public folder used for css, and other files on local host.
 app.use(express.static("public"));
+app.use(express.urlencoded()); //Parse URL-encoded bodies
 
 // Connent mongoDB using .env for enviroment varibles
-mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0.k5s63.mongodb.net/BOWS?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@cluster0.k5s63.mongodb.net/BOWS?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.set("useCreateIndex", true);
 
-// Homepage
-app.get("/", (req, res) => {
-  res.send('Hello World!');
+// Schema
+const resortSchema = new mongoose.Schema({
+  name: String,
+  location: Object,
+  price: Object
 });
+
+const resort = mongoose.model('Resort', resortSchema);
+
+//RESTFUL API
+//GET
+app.get("/", (req, res) => {
+  resort.find((err, resort) => {
+    if (!err) {
+      res.send(resort);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+//PUT
+app.post("/", (req, res) => {
+
+});
+
 
 app.listen(port, () => {
   console.log(`BOWS app listening at http://localhost:${port}`)
