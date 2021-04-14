8ea06f4ebf5a0fe6f2444141a1b624ce1670af1d
Danny Lisamay <DannyLisamay@gmail.com>
2021-04-13 23:55:10 -0400

    fixing hosting bugs

--- app.js
+++ app.js
@@ -123,13 +123,7 @@ app.route("/:resortName")
   });
 
 // Local port 4000 used for testing.
-// port (as described above) and host are both wrong
-const host = 'localhost';
-const port = 3000;
-
-// use alternate localhost and the port Heroku assigns to $PORT
-const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`BOWS app listening at http://localhost:${port}`)
 });
